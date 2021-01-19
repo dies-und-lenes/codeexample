3c33a3ecf9fb50e4a53d0abc7f691e8d6cf81280
Lena <lena.schindler@posteo.de>
2021-01-19 12:09:01 +0100

    add doc strings to the parser functions

--- src/parsenewick.jl
+++ src/parsenewick.jl
@@ -1,15 +1,30 @@
-################################################################################
-# note: this newick parser is reduced
-# - no length information is considered
-# - empty leave signifiers are not allowed
 
 
+"""
+    parsetree(str)
+
+Return a tree object from a string that represents the tree in Newick format.
+
+# Example:
+```
+parsetree("(A,B,(C,D)E);")
+```
+Note: this Newick parser is reduced
+* no length information is considered
+* empty leave signifiers are not allowed
+It is based on the grammar formulated [here](https://en.wikipedia.org/wiki/Newick_format)
+"""
 function parsetree(str::AbstractString)
     root = getroot(str)
     tree = Tree(root,[],[], Dict())
     return match_subtree(chop(str), tree, root, Vector{TreeNode}()) #remove ; from tree
 end
 
+"""
+    getroot(str)
+
+Perfom the initial parsing step: check if the string ends with a ';' and create a root object.
+"""
 function getroot(str::AbstractString)
     if last(str) == ';'
         tree_block = match(r"\(?.*\)?([\w\._/]*):?[\w\._/]*", str)
@@ -24,6 +39,9 @@ function getroot(str::AbstractString)
     end
 end
 
+"""
+Perform two alternative rules: either a subtree is a leaf or an internal node surrounded by parentheses and followed by an optional name"
+"""
 function match_subtree(str::AbstractString,
                        tree::Tree,
                        parent::TreeNode,
@@ -45,6 +63,15 @@ function match_subtree(str::AbstractString,
     return result
 end
 
+"""
+    match_internal(string, tree, parent, preds)
+
+Matches an internal node, represented by a string surrounded by parentheses and followed by an optional name
+
+# Example
+* (A,B,(C,D)E) -> A,B,(C,D)E ~ node name is 'Nn', where n is a number
+* (C,D)E -> C,D  ~  node name is 'E'
+"""
 function match_internal(str::AbstractString,
                         tree::Tree,
                         parent::TreeNode,
@@ -57,22 +84,36 @@ function match_internal(str::AbstractString,
         node = InnerNode(internal.captures[2])
     end
     push!(tree.innernodes, (node, parent))
-    match_branch(internal.captures[1], tree, node, parents)
+    match_branch(internal.captures[1], tree, node, preds)
     return tree
 end
 
+"""
+    match_branch(string, tree, parent, preds)
+
+Realizes the grammar rule that accepts any sequence of commaseparated branches, where a branch is a leaf or an internal node
+"""
 function match_branch(str::AbstractString,
                       tree::Tree,
                       parent::TreeNode,
-                      parents::Vector{TreeNode})
+                      preds::Vector{TreeNode})
     for branch in split_branch(str)
         # prepare the leavemap for each inner node
-        newparents = Vector{TreeNode}()
-        push!(newparents, parent)
-        match_subtree(branch, tree, parent, [newparents;parents])
+        newpreds = Vector{TreeNode}()
+        push!(newpreds, parent)
+        match_subtree(branch, tree, parent, [newpreds;preds])
     end
 end
 
+"""
+    split_branch(str)
+
+Return a collection of branches, where each branch consists of either a leaf or an internal node.
+Throws an error if the given string contains unbalanced parentheses
+
+# Example
+* 'A,B,(C,D)E' -> ['A','B','(C,D)E']
+"""
 function split_branch(str::AbstractString)
     branch_splits = Vector{Int64}()
     i = 0
@@ -108,12 +149,19 @@ function split_branch(str::AbstractString)
     return branches
 end
 
-function is_subtree(str::AbstractString)
-    subtree = match(r"\((?:.*,.*|(?R))*\)[\w\.:_/]*", str)
-    return subtree.match == str 
+
+"Check if a string matches the subtree pattern: comma separated leaf labels or internal nodes enclosed by balanced parentheses, optional label following a closing paren"
+function is_subtree(str::AbstractString) #unused
+    subtree = match(r"\((?:[^)(]*,[^)(]*|(?R))*\)[\w\.:_/]*", str)
+    return subtree.match == str
 end
 
-function match_leaf(str::AbstractString,
+"""
+    add_leaf(string, tree, parent, preds)
+
+Create a new entry in the leaf list of the tree and add the leaf to the leaflist of its predecessors.
+"""
+function add_leaf(str::AbstractString,
                     tree::Tree,
                     parent::TreeNode,
                     parents::Vector{TreeNode})
@@ -122,10 +170,15 @@ function match_leaf(str::AbstractString,
     add_parents(tree, Leave(str) , parents)
 end
 
-function add_parents(tree::Tree, node::Leave, parents::Vector{TreeNode})
-    for p in parents
+"""
+    add_preds(tree, leaf, preds)
+
+Replace the leaflist of all the inner nodes contained in `preds` by their leaflist that is extended by the `leaf`.
+"""
+function add_preds(tree::Tree, leaf::Leave, preds::Vector{TreeNode})
+    for p in preds
         lm = get(tree.leavemap, p, [])
-        push!(lm, node)
+        push!(lm, leaf)
         push!(tree.leavemap, p=>lm)
     end
 end
