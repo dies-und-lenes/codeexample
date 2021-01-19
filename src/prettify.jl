04635bc8c1db89c8a0b0bc81a91c87afc66bc023
Lena <lena.schindler@posteo.de>
2021-01-19 12:52:14 +0100

    include the leafmap in Base.show of the tree

--- src/prettify.jl
+++ src/prettify.jl
@@ -12,4 +12,9 @@ function Base.show(io::IO, tree::Tree)
     for node in tree.leaves
         println(io, string(node[1]) * " <- " * string(node[2]))
     end
+    println(io, "leafmap:")
+    println(io, "inner node -> leaves")
+    for nd in tree.leavemap
+        println(io, string(nd[1])* " -> " * string(nd[2]))
+    end
 end
